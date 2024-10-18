bec69b72173013dd7609a94e9907027c5a325b46
Tongludo <ductong2025@gmail.com>
2024-10-18 11:24:23 +0700

    Tuan7

---
+++ TaiKhoan.java
@@ -0,0 +1,44 @@
+public class TaiKhoan {
+    private String soTaiKhoan;
+    private String chuTaiKhoan;
+    private double soDu;
+    private String matKhau;
+
+    public TaiKhoan(String soTaiKhoan, String chuTaiKhoan, double soDu, String matKhau) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.chuTaiKhoan = chuTaiKhoan;
+        this.soDu = soDu;
+        this.matKhau = matKhau;
+    }
+
+    public void guiTien(double soTien) {
+        if (soTien > 0) {
+            soDu = soDu + soTien;
+            System.out.println("Gui thanh cong. So du hien tai: " + soDu);
+        } else {
+            System.out.println("So tien phai lon hon 0");
+        }
+    }
+
+    public void rutTien(double soTien) {
+        if (soTien > 0 && soTien <= soDu) {
+            soDu =soDu - soTien;
+            System.out.println("rut thanh cong. So du hien tai: " + soDu);
+        }else 
+            System.out.println("So tien rut khong hop le");
+
+    }
+
+    public void kiemTraSoDu() {
+        System.out.println("So du hien tai: " + soDu);
+    }
+
+    public void doiMatKhau(String matKhauCu, String matKhauMoi) {
+        if (this.matKhau.equals(matKhauCu)) {
+            this.matKhau = matKhauMoi;
+            System.out.println("Doi mat khau thannh cong");
+        }else 
+            System.out.println("Mat khau cu khong dung");
+    }
+}
+
